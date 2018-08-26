35e098f83e0eeaedd3052df2d0348f07704f1757
jbovee <Mitouch.b@gmail.com>
2018-08-26 14:59:52 -0400

    Tie label opacity to rect hover

--- src/js/popup.js
+++ src/js/popup.js
@@ -44,25 +44,31 @@ const createWindowRects = windows => {
 	let color = null;
 	windows.forEach((win,i) => {
 		color = (currentWindow.id == win.id) ? "#ff0000" : "#4CAF50";
+		const g = document.createElementNS(xmlns, "g");
+		g.setAttribute("id", win.id);
+		g.setAttribute("class", "window");
 		const rect = document.createElementNS(xmlns, "rect");
 		rect.setAttribute("id", win.id);
-		rect.setAttribute("class", "window");
 		rect.setAttribute("x", (win.left/scale));
 		rect.setAttribute("y", (win.top/scale));
 		rect.setAttribute("width", (win.width/scale));
 		rect.setAttribute("height", (win.height/scale));
 		rect.setAttribute("fill", color);
+		rect.setAttribute("fill-opacity", 0.4);
 		rect.setAttribute("stroke", color);
-		svg.appendChild(rect);
+		rect.setAttribute("stroke-opacity", 0.8);
+		g.appendChild(rect);
 		const label = document.createElementNS(xmlns, "text");
 		label.setAttribute("x", (win.left/scale) + (win.width/scale) / 2);
 		label.setAttribute("y", 8 + (win.top/scale) + (win.height/scale)/2);
 		label.setAttribute("fill", color);
+		label.setAttribute("fill-opacity", 0.8);
 		label.setAttribute("font-size", 24);
 		label.setAttribute("text-anchor", "middle");
 		label.setAttribute("alignment-baseline", "center")
 		label.textContent = `${i+1}`;
-		svg.appendChild(label);
+		g.appendChild(label);
+		svg.appendChild(g);
 		document.getElementById(win.id.toString()).addEventListener("click", () => {
 			chrome.runtime.sendMessage({type: "popup_swap", fWin: currentWindow, cWin: win});
 		});
