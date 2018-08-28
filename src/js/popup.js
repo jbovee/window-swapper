6423469386396e34aa1adf7fa11ccd944a811b76
jbovee <Mitouch.b@gmail.com>
2018-08-28 02:15:39 -0400

    Move creation of base popup page to single function

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,18 +1,49 @@
 window.onload = () => {
-	currentWindow = null,
-	otherWindow = null,
 	scale = 10,
 	xmlns = "http://www.w3.org/2000/svg";
 
-	createMiniDisplays();
-	chrome.windows.getLastFocused(getWindows);
+	document.getElementById("root").innerHTML = "";
+	loadBasePage();
 }
 
-const createMiniDisplays = () => {
+const loadBasePage = () => {
+	const root = document.getElementById("root");
+
+	const buttons = document.createElement("div");
+	buttons.setAttribute("id", "layout-buttons");
+
+	const save = document.createElement("button");
+	save.setAttribute("id", "layout-save");
+	save.innerHTML = "Save Layout";
+
+	const load = document.createElement("button");
+	load.setAttribute("id", "layout-load");
+	load.innerHTML = "Load Layout";
+
+	const manage = document.createElement("button");
+	manage.setAttribute("id", "layout-manage");
+	manage.innerHTML = "Manage Layout";
+
+	buttons.appendChild(save);
+	buttons.appendChild(load);
+	buttons.appendChild(manage);
+
+	const screens = document.createElement("div");
+	screens.setAttribute("id", "screens");
+
+	const svg = document.createElementNS(xmlns, "svg");
+	createMiniDisplays(svg);
+	createMiniWindows(svg);
+	screens.appendChild(svg);
+
+	root.appendChild(buttons);
+	root.appendChild(screens);
+}
+
+const createMiniDisplays = svg => {
 	chrome.system.display.getInfo(displays => {
 		let maxH = 0,
 			maxW = 0;
-		const svg = document.getElementById("mini-svg");
 		displays.forEach(display => {
 			const w = display.bounds.left + display.bounds.width;
 			const h = display.bounds.top + display.bounds.height;
@@ -34,48 +65,46 @@ const createMiniDisplays = () => {
 	});
 }
 
-const getWindows = focusedWindow => {
-	currentWindow = focusedWindow;
-	chrome.windows.getAll(createWindowRects);
-}
-
-const createWindowRects = windows => {
-	const svg = document.getElementById("mini-svg");
-	let color = null;
-	windows.forEach((win,i) => {
-		color = (currentWindow.id == win.id) ? "#ff0000" : "#4CAF50";
-		const g = document.createElementNS(xmlns, "g");
-		g.setAttribute("id", win.id);
-		g.setAttribute("class", "window");
-		const rect = document.createElementNS(xmlns, "rect");
-		rect.setAttribute("id", win.id);
-		rect.setAttribute("x", (win.left/scale));
-		rect.setAttribute("y", (win.top/scale));
-		rect.setAttribute("width", (win.width/scale));
-		rect.setAttribute("height", (win.height/scale));
-		rect.setAttribute("fill", color);
-		rect.setAttribute("fill-opacity", 0.4);
-		rect.setAttribute("stroke", color);
-		rect.setAttribute("stroke-width", 0.5);
-		rect.setAttribute("stroke-opacity", 0.8);
-		g.appendChild(rect);
-		const label = document.createElementNS(xmlns, "text");
-		label.setAttribute("x", (win.left/scale) + (win.width/scale) / 2);
-		label.setAttribute("y", 8 + (win.top/scale) + (win.height/scale)/2);
-		label.setAttribute("fill", "#ffffff");
-		label.setAttribute("fill-opacity", 1);
-		label.setAttribute("stroke", color);
-		label.setAttribute("stroke-width", 0.5);
-		label.setAttribute("stroke-opacity", 1);
-		label.setAttribute("font-size", 24);
-		label.setAttribute("font-weight", "bold");
-		label.setAttribute("text-anchor", "middle");
-		label.setAttribute("alignment-baseline", "center")
-		label.textContent = `${i+1}`;
-		g.appendChild(label);
-		svg.appendChild(g);
-		document.getElementById(win.id.toString()).addEventListener("click", () => {
-			chrome.runtime.sendMessage({type: "popup_swap", fWin: currentWindow, cWin: win});
+const createMiniWindows = svg => {
+	chrome.windows.getLastFocused(current => {
+		chrome.windows.getAll(windows => {
+			let color = null;
+			windows.forEach((win,i) => {
+				color = (current.id == win.id) ? "#ff0000" : "#4CAF50";
+				const g = document.createElementNS(xmlns, "g");
+				g.setAttribute("id", win.id);
+				g.setAttribute("class", "window");
+				g.addEventListener("click", () => {
+					chrome.runtime.sendMessage({type: "popup_swap", fWin: current, cWin: win});
+				});
+				const rect = document.createElementNS(xmlns, "rect");
+				rect.setAttribute("id", win.id);
+				rect.setAttribute("x", (win.left/scale));
+				rect.setAttribute("y", (win.top/scale));
+				rect.setAttribute("width", (win.width/scale));
+				rect.setAttribute("height", (win.height/scale));
+				rect.setAttribute("fill", color);
+				rect.setAttribute("fill-opacity", 0.4);
+				rect.setAttribute("stroke", color);
+				rect.setAttribute("stroke-width", 0.5);
+				rect.setAttribute("stroke-opacity", 0.8);
+				g.appendChild(rect);
+				const label = document.createElementNS(xmlns, "text");
+				label.setAttribute("x", (win.left/scale) + (win.width/scale) / 2);
+				label.setAttribute("y", 8 + (win.top/scale) + (win.height/scale)/2);
+				label.setAttribute("fill", "#ffffff");
+				label.setAttribute("fill-opacity", 1);
+				label.setAttribute("stroke", color);
+				label.setAttribute("stroke-width", 0.5);
+				label.setAttribute("stroke-opacity", 1);
+				label.setAttribute("font-size", 24);
+				label.setAttribute("font-weight", "bold");
+				label.setAttribute("text-anchor", "middle");
+				label.setAttribute("alignment-baseline", "center")
+				label.textContent = `${i+1}`;
+				g.appendChild(label);
+				svg.appendChild(g);
+			});
 		});
 	});
 }
